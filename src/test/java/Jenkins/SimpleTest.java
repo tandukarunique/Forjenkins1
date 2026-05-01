79d5e8fdf9807c316c95ff9a9a511737d06b98ca
tandukarunique <uniquetandukar143@gmail.com>
2026-05-01 16:17:28 +0545

    forjenkins1

---
+++ src/test/java/Jenkins/SimpleTest.java
@@ -0,0 +1,15 @@
+package Jenkins;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void myFirstTest() {
+        System.out.println("Hello Jenkins! This test passed.");
+    }
+
+    @Test
+    public void mySecondTest() {
+        System.out.println("Another test that also passes.");
+    }
+}
